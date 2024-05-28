7a252b2fdd3f39ef65712d60cd01bf56827e767e
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-05-28 12:30:15 -0300

    Make the relations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  has_many :livros
+  validates :name, uniqueness: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :bookcase
   belongs_to :author
+  validate :is_the_bookcase_on_limit
+
+  def is_the_bookcase_on_limit
+    if bookcase.books.count >= bookcase.limit
+      errors.add(:base, "A estante está cheia!")
+    end
+  end
 end
